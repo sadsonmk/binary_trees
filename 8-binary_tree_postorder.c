6f9dcbcc7f6984b920ae8f37261c7ee53fac6bd0
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-11-01 20:50:24 +0100

    err

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -7,7 +7,7 @@
  * Return: If tree or func is NULL, else void
  */
 
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 		return;
