a7119f4328d1b6dab71f551db3bd997564876ce1
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-11-01 20:52:40 +0100

    traverses a binary tree with post-order.

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -11,7 +11,7 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 		return;
-	func(tree->n);
 	binary_tree_preorder(tree->left, func);
 	binary_tree_preorder(tree->right, func);
+	func(tree->n);
 }
