1fe39a08a078a4bbedbe348a7889d1392261e7c4
Sadson Mk <sadsonmkumira@yahoo.com>
2023-11-02 09:34:16 -0700

    Adding balance factor of a binary tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,5 @@
 #include "binary_trees.h"
 
-int _max(int x, int y);
 /**
  * binary_tree_balance - measures the balance factor of a binary tree
  * @tree: pointer to the root node of the tree to measure the balance factor
@@ -10,25 +9,15 @@ int _max(int x, int y);
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int counter1 = 0, counter2 = 0;
 
 	if (tree == NULL)
 		return (0);
+	if (tree->left)
+		counter1 = binary_tree_balance(tree->left) + 1;
+	if (tree->right)
+		counter2 = binary_tree_balance(tree->right) + 1;
 
 
-	return (binary_tree_balance(tree->left) - binary_tree_balance(tree->right));
-}
-
-/**
- * _max - calculates the maximum of two integers
- * @x: is the first input of the two ints
- * @y: is the second input of the two ints
- *
- * Return: the bigger one of the the two inputs
- */
-
-int _max(int x, int y)
-{
-	if (x > y)
-		return (x);
-	return (y);
+	return (counter1 - counter2);
 }
