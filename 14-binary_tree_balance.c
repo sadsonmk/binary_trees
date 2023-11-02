e37d8cdcb148d7f77ff6a328d9f3e7d71f83834b
Sadson Mk <sadsonmkumira@yahoo.com>
2023-11-02 10:09:33 -0700

    Adding balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,12 +9,33 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int counter1 = 0, counter2 = 0;
-
 	if (tree == NULL)
 		return (0);
-	counter1 = binary_tree_height(tree->left);
-	counter2 = binary_tree_height(tree->right);
 
-	return (counter1 - counter2);
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+}
+
+/**
+ * binary_tree_height -  measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height
+ * Return: height of the binary_tree
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t counter1, counter2;
+
+	counter1 = 0;
+	counter2 = 0;
+	if (tree == NULL)
+		return (0);
+	if (tree->left)
+		counter1 = binary_tree_height(tree->left) + 1;
+
+	if (tree->right)
+		counter2 = binary_tree_height(tree->right) + 1;
+
+	if (counter1 > counter2)
+		return (counter1);
+	return (counter2);
 }
