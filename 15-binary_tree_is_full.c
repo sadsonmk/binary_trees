bd794bb9ff571a6def37a41d7f59663195bf00d6
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-11-03 03:55:02 +0100

    function measures the size of a binary tree

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -36,8 +36,8 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	{
 		if (tree->right == NULL && tree->left == NULL)
 			size += 1;
-		size += binary_tree_leaves(tree->left);
 		size += binary_tree_leaves(tree->right);
+		size += binary_tree_leaves(tree->left);
 	}
 	return (size);
 }
