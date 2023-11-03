4af8a1ce2909033592e309e41beacac990143859
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-11-03 03:45:37 +0100

    function counts the nodes with a least 1 child

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - function counts the nodes with a least 1 child
+ * @tree: pointer to the root node of the tree to count
+ * Return: 0 if tree is NULL else counts
+ */
+
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t counter = 0;
+
+	if (tree)
+	{
+		if (tree->left != NULL || tree->right != NULL)
+			count += 1;
+		counter += binary_tree_nodes(tree->left);
+		counter +=  binary_tree_nodes(tree->right);
+	}
+
+	return (count);
+}
